2ece98e4218a7417f59bfb2b52fc34fba1ebcd3e
ram.adhan <ram.adhan@outlook.com>
2020-01-21 10:42:32 +0700

    Update file rocket

--- native-livecode-week5/rocket.js
+++ native-livecode-week5/rocket.js
@@ -16,7 +16,76 @@
 
 function findSpecials(array) {
     // your code here
-    
+    var resultArray = [];
+    var mean = [], modus = [], max = [];
+
+    //// MEAN
+    for (i = 0; i < array.length; i++) {
+        mean[i] = 0;
+        for (j = 0; j < array[i].length; j++) {
+            mean[i] += array[i][j];
+        }
+        mean[i] /= array[i].length;
+    }
+    // return mean;
+    ////
+
+    //// MODUS
+    for (i = 0; i < array.length; i++) {
+        var list = [];
+        for (j = 0; j < array[i].length; j++) {
+            var alreadyInList = false;
+            for (k = 0; k < list.length; k++) {
+                if (array[i][j] === list[k][0]) {
+                    alreadyInList = true;
+                    list[k][1]++;
+                }
+            }
+            if (!alreadyInList) {
+                list.push([array[i][j], 1]);
+            }
+        }
+        // console.log(list);
+        
+        var temp;
+        for (l = 0; l < list.length-1; l++) {
+            for (m = 0; m < list.length-1-l; m++) {
+                if (list[m][1] < list[m+1][1] && list[m][0] > list[m+1][0]) {
+                    temp = list[m];
+                    list[m] = list[m+1];
+                    list[m+1] = temp;
+                }
+            }
+        }
+        // console.log(list);
+        modus.push(list[0][0]);
+    }
+    // return modus;
+    ////
+
+    //// MAX
+    for (i = 0; i < array.length; i++) {
+        var tmp;
+        for (j = 0; j < array[i].length-1; j++) {
+            for (k = 0; k < array[i].length-1-j; k++) {
+                if (array[i][k] < array[i][k+1]) {
+                    tmp = array[i][k];
+                    array[i][k] = array[i][k+1];
+                    array[i][k+1] = tmp;
+                }
+            }
+        }
+        max.push(array[i][0]);
+    }
+    // return max;
+    ////
+
+    //// RESULT
+    for (i = 0; i < array.length; i++) {
+        resultArray.push([mean[i],modus[i],max[i]]);
+    }
+    return resultArray;
+    ////
 }
 
 
